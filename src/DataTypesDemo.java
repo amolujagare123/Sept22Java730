3d109510f508e4a7cf448e30dfcf538827d60d4d
amolu <amolujagare@gmail.com>
2022-09-12 20:35:56 +0530

    Initial commit

---
+++ src/DataTypesDemo.java
@@ -0,0 +1,29 @@
+public class DataTypesDemo {
+
+    public static void main(String[] args) {
+
+        byte b = -128;
+
+        short s = -32768;
+
+        float f = 34.5f ;
+
+        double d = 76.56 ;
+
+        boolean result1 = true;
+        boolean result2 = false;
+
+        char c = '@';
+
+
+      //  int a1 = 12.45;  // not possible
+
+        double d1 = 90; // possible
+
+        char c1 = 98 ; // possible
+        int a2 = 'y' ; // possible
+
+        System.out.println(c1);
+        System.out.println(a2);
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Amol is a java trainer");
+        System.out.println("We are learning selenium");
+        System.out.println("this is a java class");
+    }
+}
